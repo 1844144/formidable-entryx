e459db378e00085d5eba5ba044dffad855fa1ba5
vl <vl@u64>
2015-11-17 09:20:10 +0300

    Initial

---
+++ controllers/EntryxController.php
@@ -0,0 +1,81 @@
+<?php
+
+
+if (!function_exists(evar_dump)) {
+function evar_dump( $object=null ){
+    ob_start();                    // start buffer capture
+    var_dump( $object );           // dump the values
+    $contents = ob_get_contents(); // put the buffer into a variable
+    ob_end_clean();                // end capture
+    error_log( $contents );        // log contents of the result of var_dump( $object )
+}
+}
+
+
+
+function do_entry_x($new_content, $entry, $shortcodes, $display, $show, $odd, $atts){
+
+    // $new_content = '14 5 [entry_x 3] hello [/entry_x] 6 7';
+    // $atts['count'] = 4;
+    // replace all unneded spaces
+
+    $entryx = '[/entry_x]';
+
+    $new_content = preg_replace('/\[\/entry_x\ +\]/', '[/entry_x]', $new_content);
+
+    $start = 0;
+    $my = '';
+    while (True) {
+        $pos = strpos($new_content, $entryx);
+        if (!$pos) break;
+
+
+
+        // now get substring and process it
+        $sub = substr($new_content, 0, $pos+strlen($entryx));
+        $array = preg_match('/\[entry_x\ *(\d+)\ *\](.*)\[\/entry_x\]/', $sub, $mat);
+            
+        if ($array) {
+            $xx = $mat[1];
+            $sc_content = '';
+            
+            if ($atts['count'] % $xx == 0) {
+                $sc_content = $mat[2];
+            }
+            $sub = preg_replace( '/(\[entry_x.*\[\/entry_x\])/', $sc_content, $sub);
+
+            // now glue new sub and old content
+            $new_content = $sub. substr($new_content, $pos+strlen($entryx));
+        }
+
+    }
+    evar_dump($new_content);
+    return $new_content;
+
+}
+
+
+class EntryxController{
+    function __construct(){
+        add_action( 'init', 'EntryxController::load_hooks', 10 );
+        // add_action( 'admin_init', 'FrmSigAppController::include_updater', 1 );
+        // add_action('after_setup_theme', 'FrmCronAppController::addMyHooks');
+    }
+
+
+
+    public static function load_hooks(){
+        add_filter('frm_display_entry_content', 'do_entry_x', 20, 7);
+
+
+
+    }
+
+
+
+    public static function path(){
+        return WP_PLUGIN_DIR .'/formidable-entryx';
+    }
+
+
+}
---
+++ signature.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Plugin Name: Formidable Entry X
+Description: Adds [entry_x] shortcode. Content of shortcode will appear in x-th entries. 
+Version: 0.0.1 
+Plugin URI: 
+Author URI: 
+Author: 1844144@gmail.com
+Text domain: 
+*/
+// Instansiate Controllers
+include_once(dirname( __FILE__ ) . '/controllers/EntryxController.php');
+$obj = new EntryxController();
+unset($obj);
+
+
+
